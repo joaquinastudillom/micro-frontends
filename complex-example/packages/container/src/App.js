bb774573b5be7f009f4dddd0669f54ce89c4a9d6
Joaquin Astudillo <joaquin.alonso.astudillo@gmail.com>
2023-06-09 16:42:31 +0100

    Testing deploy #2 on container

--- complex-example/packages/container/src/App.js
+++ complex-example/packages/container/src/App.js
@@ -3,7 +3,7 @@ import { MarketingApp } from './components/MarketingApp';
 
 export const App = () => (
     <div>
-        <h1>Hi there, first run!</h1>
+        <h1>Hi there, second run!</h1>
         <hr />
         <MarketingApp />
     </div>
